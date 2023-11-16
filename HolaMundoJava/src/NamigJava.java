82d5f12df7d0cedbf0d2b470e4c3ff224ce0b10d
Karyjb <kary.jimenenez1127@gmail.com>
2023-11-15 22:41:27 -0500

    Revert "archivos v1"
    This reverts commit e7e6dbbff3706d5efdfbe26ee1739b56bce4baba.

--- HolaMundoJava/src/NamigJava.java
+++
@@ -1,9 +0,0 @@
-            //Upper Camel Case
-public class NamigJava {
-    public static void main(String[] args) {
-        int cellphone = 314424422;
-    //lower Camel Case
-        String fullName="Andrea Karina Jimenez";
-
-    }
-}
